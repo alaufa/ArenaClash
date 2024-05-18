84b591377d7c3ca7df9483754bf5e3123d4da73d
Abdurrahman Al Aufa <aufa0987654321@gmail.com>
2024-05-18 20:46:05 +0700

    FIx Michael jackson walk

--- Assets/PlayerMovement1.cs
+++ Assets/PlayerMovement1.cs
@@ -10,9 +10,12 @@ public class PlayerMovement2 : MonoBehaviour {
     public float runSpeed = 40f;
 
     float horizontalMove = 0f;
-    bool jump = false;  // Changed from true to false
+    bool jump = false;
     bool crouch = false;
-    
+
+    // To keep track of facing direction
+    private bool facingRight = true;
+
     // Update is called once per frame
     void Update () {
         horizontalMove = Input.GetAxisRaw("Horizontal") * runSpeed;
@@ -22,7 +25,6 @@ public class PlayerMovement2 : MonoBehaviour {
         if (Input.GetButtonDown("Jump2"))
         {
             jump = true;
-            Debug.Log("Jump button pressed");
         }
 
         if (Input.GetButtonDown("Crouch"))
@@ -39,5 +41,28 @@ public class PlayerMovement2 : MonoBehaviour {
         // Move our character
         controller.Move(horizontalMove * Time.fixedDeltaTime, crouch, jump);
         jump = false;
+
+        // Flip the character if needed
+        Flip(horizontalMove);
+    }
+
+    void Flip(float moveDirection)
+    {
+        if (moveDirection > 0 && !facingRight)
+        {
+            // Right direction and currently facing left, flip the character
+            facingRight = true;
+            Vector3 theScale = transform.localScale;
+            theScale.x = Mathf.Abs(theScale.x); // Ensure x is positive
+            transform.localScale = theScale;
+        }
+        else if (moveDirection < 0 && facingRight)
+        {
+            // Left direction and currently facing right, flip the character
+            facingRight = false;
+            Vector3 theScale = transform.localScale;
+            theScale.x = -Mathf.Abs(theScale.x); // Ensure x is negative
+            transform.localScale = theScale;
+        }
     }
 }
